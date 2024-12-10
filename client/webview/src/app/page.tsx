bad88dd9ac9eaadec8b98f90be30cbb83b0a5522
katana <736652384@qq.com>
2024-12-10 10:09:57 +0800

    fix page

--- client/webview/src/app/page.tsx
+++ client/webview/src/app/page.tsx
@@ -6,20 +6,55 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.home}>
-        <Image
-          src="/favicon.png"
-          alt="next-admin"
-          width={120}
-          height={60}
-          style={{borderRadius: 6}}
-          priority
-        />
-        <div className={styles.content}>
+      <Image
+        src="/favicon.png"
+        alt="next-admin"
+        width={120}
+        height={60}
+        style={{ borderRadius: 6 }}
+        priority
+      />
+      <div className={styles.content}>
+        <p>
+          desc
+        </p>
 
-          <div><Link href="/dashboard"><Button type="primary">尝试</Button></Link></div>
+        <h2>log.title</h2>
+
+        <div className={styles.timeBox}>
+          <Timeline
+            items={[
+              {
+                children: 'log.1',
+              },
+              {
+                children: 'log.2',
+              },
+              {
+                children: 'log.3',
+              },
+              {
+                color: 'orange',
+                children: 'log.4',
+              },
+              {
+                color: 'orange',
+                children: 'log.5',
+              },
+              {
+                color: 'orange',
+                children: 'log.6',
+              },
+              {
+                color: 'orange',
+                children: 'log.7',
+              }
+            ]}
+          />
         </div>
-        
-      
+
+        <div><Link href="/dashboard"><Button type="primary">尝试</Button></Link></div>
+      </div>
     </main>
   );
 }
