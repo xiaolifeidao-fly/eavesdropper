46c5c52c6f3fde7c1ce7c50c200772dbf6394424
katana <736652384@qq.com>
2024-12-10 10:55:12 +0800

    webview:登录页面

---
+++ client/webview/src/app/auth/login/api.ts
@@ -0,0 +1,3 @@
+// import req from '@/utils/req';
+
+export const loginApi = async (email: string, pwd: string) => console.log('loginApi', email, pwd)
---
+++ client/webview/src/app/auth/login/page.tsx
@@ -0,0 +1,142 @@
+'use client'
+import { Button, Form, Input, Segmented, type FormProps } from 'antd';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { loginApi } from './api';
+import Image from 'next/image';
+
+import styles from './index.module.less';
+
+type FieldType = {
+  username?: string;
+  password?: string;
+};
+
+const mode = ['登录', '注册'];
+
+export default function Home() {
+  const [curMode, setCurMode] = useState(mode[0]);
+  const [form] = Form.useForm();
+  const router = useRouter();
+
+  const onFinish: FormProps<FieldType>["onFinish"] = (values: any) => {
+    // 登录
+    if (curMode === mode[0]) {
+      const { username, password } = values;
+      loginApi(username, password).then(res => {
+        // login logic
+        // router.push('/dashboard');
+        console.log('loginApi success');
+      })
+      return
+    }
+
+    // 注册
+    if (curMode === mode[1]) {
+      console.log('registerApi', values);
+    }
+  };
+
+  // 登录表单
+  const LoginForm = () => (
+    <>
+      <Form.Item<FieldType>
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: '请输入账号',
+          },
+        ]}
+      >
+        <Input placeholder='请输入账号' size='large' variant="filled" />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        name="password"
+        rules={[{ required: true, message: '请输入密码' }]}
+      >
+        <Input.Password size='large' placeholder='请输入密码' variant="filled" />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ span: 24 }}>
+        <Button type="primary" htmlType="submit" block size='large'>
+          登录
+        </Button>
+      </Form.Item>
+    </>
+  );
+
+
+  // 注册表单
+  const RegisterForm = () => (
+    <>
+      <Form.Item<FieldType>
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: '请输入账号',
+          },
+        ]}
+      >
+        <Input placeholder='请输入账号' size='large' variant="filled" />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        name="password"
+        rules={[{ required: true, message: '请输入密码' }]}
+      >
+        <Input.Password size='large' placeholder='请输入密码' variant="filled" />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ span: 24 }}>
+        <Button type="primary" htmlType="submit" block size='large'>
+          注册
+        </Button>
+      </Form.Item>
+    </>
+  );
+
+
+  return (
+    <main className={styles.loginWrap}>
+      {/* 左侧banner */}
+      <div className={styles.leftBanner}>
+        <div className={styles.banner}>
+          <Image src="/logo_bg.svg" alt="" width={500} height={500} />
+        </div>
+      </div>
+
+      {/* 右侧内容 */}
+      <div className={styles.content}>
+        <div className={styles.innerContent}>
+          <h1>欢迎登录 TaoTao 中后台管理系统</h1>
+          <Segmented<string>
+            options={mode.map(item => ({ label: item, value: item, disabled: item === mode[1] }))} // 注册按钮禁用
+            size="large"
+            onChange={(value) => {
+              setCurMode(value);
+              form.resetFields();
+            }}
+          />
+          <Form
+            name="basic"
+            className={styles.form}
+            wrapperCol={{ span: 24 }}
+            style={{ maxWidth: 420 }}
+            form={form}
+            onFinish={onFinish}
+            initialValues={{
+              email: 'dooring@next.com',
+              pwd: 'dooring.vip'
+            }}
+            autoComplete="off"
+          >
+            {curMode === mode[0] ? <LoginForm /> : <RegisterForm />}
+          </Form>
+        </div>
+      </div>
+    </main>
+  );
+}
