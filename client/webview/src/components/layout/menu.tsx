1cae8ce79f2309510678fde5dd7887de0c26184c
xianglong <xianglong@wacai.com>
2024-12-19 17:44:36 +0800

    fix

---
+++ client/webview/src/components/layout/menu.tsx
@@ -0,0 +1,42 @@
+import { 
+    FundOutlined, 
+    LayoutOutlined, 
+    BarChartOutlined, 
+    DesktopOutlined, 
+    ScheduleOutlined, 
+    CalculatorOutlined, 
+    UserOutlined, 
+    WalletOutlined,
+    BuildOutlined 
+} from '@ant-design/icons';
+import React from 'react';
+
+const getNavList = () => {
+    return [
+        {
+            key: '/dashboard',
+            icon: <DesktopOutlined />,
+            label: '主页'
+        },
+        {
+            key: '/auth',
+            icon: <UserOutlined />,
+            label: '用户中心',
+            children: [
+                {
+                    key: '/auth/user',
+                    icon: <UserOutlined />,
+                    label: '用户管理'
+                }
+            ]
+        },
+        {
+            key: '/shop',
+            icon: <LayoutOutlined />,
+            label: '商品管理'
+        },
+        
+    ]
+}
+
+export default getNavList
