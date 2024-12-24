da8de2dffda926c90a151871fdc9b3deb46f3202
xianglong <xianglong@wacai.com>
2024-12-24 10:51:48 +0800

    fix

--- client/app/src/impl/door/sku/sku.ts
+++ client/app/src/impl/door/sku/sku.ts
@@ -34,23 +34,33 @@ export class MbSkuApiImpl extends MbSkuApi {
         if(!page){ 
             return undefined;
         }
+        //获取商品信息
         const skuData = await this.findMbSkuInfo(skuUrl);
+        //上传图片信息
 
 
-        // const monitor = new MbShopInfoMonitor();
+        // 填充商品信息
         const result = await engine.doFillWaitForElement(page, "1.0.1", "publishSku", skuData);
+        // 返回发布结果
         return new Sku(undefined, undefined, undefined, undefined);
     }
 
     @InvokeType(Protocols.INVOKE)
     async batchPublishShops(publishResourceId : number, skuUrls : string[]) : Promise<Task|undefined>{
+        // 创建任务要求是幂等
+        
+        
+        // 异步操作
         for(const skuUrl of skuUrls){
+            //发布商品
             const result = await this.publishShop(publishResourceId, skuUrl);
             if(!result){
                 return undefined;
             }
+            //通过事件发送给端 单个商品的结果以及进度
             this.send("notifyPublishShop", result.id, result.status, new SkuPublishStatitic());
         }
+        //返回任务
         return new Task(undefined, undefined);
     }
 
