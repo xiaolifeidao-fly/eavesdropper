e173bf178b4c1482123e5c85eaeb4886bf37f6d0
xianglong <xianglong@wacai.com>
2025-03-05 11:58:47 +0800

    1

--- ai/business/switch_value.py
+++ ai/business/switch_value.py
@@ -11,8 +11,8 @@ class SwitchValueProcessor(AiProcessor):
 <data>
 {data}
 <rule>
-1.ori_data是正确的数据, 我们要补充target_data的value,修正text,以对应的correct_text为准
-2.不改变target_data原有的数组长度
+1.ori_data是正确的数据, 我们要补充target_data的code,修正value,以对应的correct_value为准
+2.不改变target_data原有的长度
 <result_style>
 不要过程,只要最终的json数据
 {result_style}
@@ -24,7 +24,7 @@ class SwitchValueProcessor(AiProcessor):
             "data" : json.dumps(params['data'], ensure_ascii=False),    
             "result_style" : json.dumps([{
                 "pid" : "",
-                "target_data" : {"text" : "", "value" : ""}
+                "target_data" : [{"code" : "", "value" : ""}]
             }], ensure_ascii=False)
         }
         return data
