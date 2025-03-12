15e3ab94d96827129d7540303cbee27c444f3104
katana <736652384@qq.com>
2025-03-12 18:05:33 +0800

    发布配置合并

--- client/webview/src/app/sku/components/SkuPushConfig.tsx
+++ client/webview/src/app/sku/components/SkuPushConfig.tsx
@@ -1,15 +1,12 @@
 'use client'
-import React, { useEffect, useState, useRef, MutableRefObject } from 'react';
-import { Button, Space, Steps, message } from 'antd';
+import React, { useState, MutableRefObject } from 'react';
+import { Space } from 'antd';
 import type { ProFormInstance } from '@ant-design/pro-components';
 import { ProFormSelect, ProCard, ProFormList, ProForm, ProFormDigit, ProFormGroup, ProFormMoney } from '@ant-design/pro-components';
 import { CloseCircleOutlined, SmileOutlined } from '@ant-design/icons';
 
 import { getShopList } from '@api/shop/shop.api';
 import { PriceRangeConfig } from "@model/sku/skuTask";
-
-const { Step } = Steps;
-
 export interface SukPushConfigProp {
   setSourceAccount: (account: number) => void, // 资源账号
   priceRangeConfigFormRef: MutableRefObject<ProFormInstance | undefined>,
@@ -18,161 +15,109 @@ export interface SukPushConfigProp {
 const SukPushConfig: React.FC<SukPushConfigProp> = (props) => {
 
   const [account, setAccount] = useState<number>();
-  const [current, setCurrent] = useState(0);
-
-  const stepTitles = [
-    "请选择资源账号",
-    "加价区间"
-  ];
 
   return (
     <ProCard
       bordered
       style={{
         height: '100%', // 确保 ProCard 的高度充满容器
+        overflow: 'auto',
       }}
     >
-      <ProCard
-        colSpan={4}
+      <ProFormSelect
+        name="sourceAccoun"
+        label="资源账号"
+        placeholder="请选择资源账号"
+        fieldProps={{ value: account }}
+        onChange={(value: number) => {
+          setAccount(value);
+          props.setSourceAccount(value);
+        }}
+        request={async () => {
+          const shopList = await getShopList();
+          const sourceList: { value: number, label: string }[] = [];
+          for (const shop of shopList) {
+            sourceList.push({
+              value: shop.resourceId,
+              label: shop.name
+            })
+          }
+          if (sourceList.length !== 0) {
+            console.log(sourceList[0]);
+            setAccount(sourceList[0].value);
+            props.setSourceAccount(sourceList[0].value);
+          }
+          return sourceList
+        }}
+        required
+      />
+      <Space>加价区间</Space>
+      <ProForm
+        formRef={props.priceRangeConfigFormRef}
+        submitter={{ render: (pr, doms) => { return [] } }}
+        initialValues={
+          {
+            priceRangeList: [{
+              minPrice: 0.01,
+              maxPrice: 1000,
+              priceMultiplier: 1.15,
+              fixedAddition: 0,
+              roundTo: "fen"
+            }]
+          }
+        }
       >
-        <Steps
-          direction={"vertical"}
-          size="small"
-          current={current}
+        <ProFormList<PriceRangeConfig>
+          name="priceRangeList"
+          copyIconProps={{ Icon: SmileOutlined, tooltipText: '复制此项到末尾' }}
+          deleteIconProps={{
+            Icon: CloseCircleOutlined,
+            tooltipText: '不需要这行了',
+          }}
         >
-          {stepTitles.map((title, index) => (
-            <Step key={index} title={title} />
-          ))}
-        </Steps>
-        <Space>
-          <Button
-            key="pre"
-            onClick={() => setCurrent(current - 1)}
-            disabled={current === 0}
-          >
-            上一步
-          </Button>
-          <Button
-            key="next"
-            type="primary"
-            onClick={() => {
-              if (current == 0) {
-                if (!account || account === 0) {
-                  message.error('请选择资源账号');
-                  return;
-                }
-              }
-              setCurrent(current + 1)
-            }}
-            disabled={current === stepTitles.length - 1}
-          >
-            下一步
-          </Button>
-        </Space>
-      </ProCard>
-      <ProCard>
-        {
-          current === 0 &&
-          <>
-            <ProFormSelect
-              name="sourceAccoun"
-              label="资源账号"
-              placeholder="请选择资源账号"
-              fieldProps={{value: account}}
-              onChange={(value: number) => {
-                setAccount(value);
-                props.setSourceAccount(value);
-              }}
-              request={async () => {
-                const shopList = await getShopList();
-                const sourceList: {value: number, label: string}[] = [];
-                for (const shop of shopList) {
-                  sourceList.push({
-                    value: shop.resourceId,
-                    label: shop.name
-                  })
-                }
-                if (sourceList.length !== 0) {
-                  console.log(sourceList[0]);
-                  setAccount(sourceList[0].value);
-                  props.setSourceAccount(sourceList[0].value);
-                }
-                return sourceList
-              }}
-              required
+          <ProFormGroup key="group">
+            <ProFormMoney
+              width="sm"
+              name="minPrice"
+              label="最小值"
+              min={0}
+              fieldProps={{ precision: 2 }}
             />
-          </>
-        }
-        {
-          current === 1 &&
-          <>
-            <ProForm
-              formRef={props.priceRangeConfigFormRef}
-              submitter={{ render: (pr, doms) => { return [] } }}
-              initialValues={
-                {priceRangeList : [{
-                  minPrice: 0.01,
-                  maxPrice: 1000,
-                  priceMultiplier: 1.15,
-                  fixedAddition: 0,
-                  roundTo: "fen"
-                }]}
-              }
-            >
-              <ProFormList<PriceRangeConfig>
-                name="priceRangeList"
-                copyIconProps={{ Icon: SmileOutlined, tooltipText: '复制此项到末尾' }}
-                deleteIconProps={{
-                  Icon: CloseCircleOutlined,
-                  tooltipText: '不需要这行了',
-                }}
-              >
-                <ProFormGroup key="group">
-                  <ProFormMoney
-                    width="sm"
-                    name="minPrice"
-                    label="最小值"
-                    min={0}
-                    fieldProps={{ precision: 2 }}
-                  />
-                  <ProFormMoney
-                    width="sm"
-                    name="maxPrice"
-                    label="最大值"
-                    min={0}
-                    fieldProps={{ precision: 2 }}
-                  />
-                  <ProFormDigit
-                    width="xs"
-                    name="priceMultiplier"
-                    label="价格乘率"
-                    min={0}
-                    max={10}
-                    fieldProps={{ precision: 2 }}
-                  />
-                  <ProFormMoney
-                    width="sm"
-                    name="fixedAddition"
-                    label="加上"
-                    min={0}
-                    fieldProps={{ precision: 2 }}
-                  />
-                  <ProFormSelect
-                    width="xs"
-                    name="roundTo"
-                    label="保留单位"
-                    options={[
-                      { value: 'yuan', label: '元' },
-                      { value: 'jiao', label: '角' },
-                      { value: 'fen', label: '分' },
-                    ]}
-                  />
-                </ProFormGroup>
-              </ProFormList>
-            </ProForm>
-          </>
-        }
-      </ProCard>
+            <ProFormMoney
+              width="sm"
+              name="maxPrice"
+              label="最大值"
+              min={0}
+              fieldProps={{ precision: 2 }}
+            />
+            <ProFormDigit
+              width="xs"
+              name="priceMultiplier"
+              label="价格乘率"
+              min={0}
+              max={10}
+              fieldProps={{ precision: 2 }}
+            />
+            <ProFormMoney
+              width="sm"
+              name="fixedAddition"
+              label="加上"
+              min={0}
+              fieldProps={{ precision: 2 }}
+            />
+            <ProFormSelect
+              width="xs"
+              name="roundTo"
+              label="保留单位"
+              options={[
+                { value: 'yuan', label: '元' },
+                { value: 'jiao', label: '角' },
+                { value: 'fen', label: '分' },
+              ]}
+            />
+          </ProFormGroup>
+        </ProFormList>
+      </ProForm>
     </ProCard>
   );
 }
